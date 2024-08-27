c47005b1548fe230bc1a8f8dadc4471c62b04c0b
17joaopimenta <joaopedropiment@gmail.com>
2024-08-26 21:18:19 -0300

    Renato is king

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,15 +7,15 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject organ;
 
     private void Update()
-    {
+    {/*
         organ = GameObject.FindGameObjectWithTag("Organ");
         
         if (organ == null) 
             Debug.Log("Legal :)");
 
         organ.SetActive(Organ.instance.disableObject);
-        /*else
-        {*/
-        //} 
+        *//*else
+        {*//*
+        //} */
     }
 }
